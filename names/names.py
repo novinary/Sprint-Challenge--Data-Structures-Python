6b354098c02505e5fc10a5c021863ff9d96bb436
novinary <novina.dev@gmail.com>
2019-06-14 10:27:00 +0100

    Optimised names.py for o(n) with runtime of 0.157 seconds

--- names/names.py
+++ names/names.py
@@ -10,11 +10,20 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+join_names = names_1 + names_2
+cache = dict()
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for name in join_names:
+    try:
+        if cache[name] == 1:
+            cache[name] += 1
+            duplicates.append(name)
+        elif cache[name] > 1:
+            continue
+    except KeyError as e:
+        cache[name] = 1
+        continue
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
